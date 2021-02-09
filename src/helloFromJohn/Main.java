6d9d746aa62b28ca89e21eb92bc62a97798f6094
jmounteer <mounteerjaden@gmail.com>
2021-02-09 07:31:14 -0700

    Add greeting from Jaden.

--- src/helloFromJohn/Main.java
+++ src/helloFromJohn/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("Hello, my name is John");
         System.out.println("Stretch Challenge test");
+        System.out.println("Hello John! This is Jaden!");
     }
 }
